a7ceae4e1436b105ddb559caab9dcb2491276952
Bisman <bismanbedi2006@gmail.com>
2025-07-15 12:40:57 +0530

    Day 1

---
+++ day1/day1.py
@@ -0,0 +1,19 @@
+#model
+#single value container
+#multi value container
+
+#create statement
+instagram_user_name="Bisman"
+#LHS:refrence variable(name of container)
+#RHS:value or data
+
+tree_age=1000
+
+folowers=1000
+#LHS:refrence variable(name of container)
+#RHS:value or data
+
+#read statement
+print (instagram_user_name)
+print (folowers, id(folowers))
+print (tree_age,id(tree_age))
---
+++ day1/multi.py
@@ -0,0 +1,15 @@
+#multi value container
+
+haris_folowers = "Bisman", "Hari", "Sita", "Gita"
+ipl_scores = 100, 200, 300, 400, 500
+menu = "Pizza",200, "Burger",30 , "Pasta", 69
+
+print(haris_folowers)
+print('------')
+print('haris_followers hashcode',id(haris_folowers))
+print('haris_followers type',type(haris_folowers))
+
+print(ipl_scores)
+print('------')
+print('ipl_score hashcode',id(ipl_scores))
+print('ipl_score type',type(ipl_scores))
