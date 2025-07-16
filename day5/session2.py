46855004f3a7ab29b73464dbe4cf41d43a95478c
Bisman <bismanbedi2006@gmail.com>
2025-07-17 02:01:19 +0530

    Day5

---
+++ day5/session2.py
@@ -0,0 +1,15 @@
+brick_by_coustomer= int(input("enter the number of coustomer:"))
+print('wall must be constructed with',brick_by_coustomer, 'bricks')
+
+total_bricks = 0
+
+john=brick_by_coustomer
+total_bricks += john
+
+
+if total_bricks > brick_by_coustomer:
+    print('john placed last brick:', total_bricks)
+    break
+for row in range(0, 5, 1):
+print("wall constructed with", total_bricks, "bricks")
+    
